a31b39a0a54b224cf3a0f0b301fafdbf9f9e64b4
myron.davis <myron@dizari.com>
2020-07-20 18:55:54 +0100

    Temporary debug change

--- Unzipthis.cs
+++ Unzipthis.cs
@@ -40,13 +40,13 @@ namespace AzUnzipEverything
                                 log.LogInformation($"Now processing {entry.FullName}");
 
                                 //Replace all NO digits, letters, or "-" by a "-" Azure storage is specific on valid characters
-                                string valideName = Regex.Replace(entry.Name,@"[^a-zA-Z0-9\-]","-").ToLower();
+                                /*string valideName = Regex.Replace(entry.Name,@"[^a-zA-Z0-9\-]","-").ToLower();
 
                                 CloudBlockBlob blockBlob = container.GetBlockBlobReference(valideName);
                                 using (var fileStream = entry.Open())
                                 {
                                     await blockBlob.UploadFromStreamAsync(fileStream);
-                                }
+                                }*/
                             }
                         }
                     }
